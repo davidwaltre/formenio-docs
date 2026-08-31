5d7228fd6dab264767cc23351116e41bedeac726
walter_d <david.walter@dlr.rlp.de>
2026-08-31 11:14:48 +0200

    reworked docs page & architecture

---
+++ docs/assets/js/5e95c892.305c40d3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkformenio_docs=self.webpackChunkformenio_docs||[]).push([["1668"],{6569(e,r,s){s.r(r),s.d(r,{default:()=>t});var c=s(4848);s(6540);var o=s(4164),a=s(4308),n=s(8287),u=s(2831),d=s(1085);function t(e){return(0,c.jsx)(a.e3,{className:(0,o.A)(n.G.wrapper.docsPages),children:(0,c.jsx)(d.A,{children:(0,u.v)(e.route.routes)})})}}}]);
